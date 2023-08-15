7dbe350a86f06483d1a3189132f540c46be5e2a5
khayo254 <khayo>
2023-08-15 22:50:07 +0300

    #241

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+def max_integer(my_list=[]):
+    if my_list is NOne:
+        return
+    if len(my_list) == 0:
+        return
+    max = my_list[0]
+    for i in my_list:
+        if i > max:
+            max = 1
+        return max
