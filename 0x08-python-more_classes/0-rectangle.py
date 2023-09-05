8e5a2344ef956fb662fc85fc3ef3ea198ddf7848
khayo254 <khayo1998@gmail.com>
2023-09-05 07:18:51 -0700

    rectangle height width

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 class Rectangle:
     pass
+def __init__(self, name):
+    self.name = name
+def __str__(self):
+    return f"{self.name}"
 my_rectangle = Rectangle()
 print(type(my_rectangle))
 print(my_rectangle.__dict__)
